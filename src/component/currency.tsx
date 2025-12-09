4c44da69324e8407c9da6c418504ef822782f5c0
Sebastien CHAUVEL <monsieurchauvel22@gmail.com>
2025-12-09 17:17:13 +0100

    step 2 > Add display button in Currency component

--- src/component/currency.tsx
+++ src/component/currency.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { ICurrency } from "../data/currencies";
+import DisplayRateButton from "./displayRateButton";
 
 export default function Currency(
   { currency, currentCurrency, setCurrentCurrency } 
@@ -6,12 +8,31 @@ export default function Currency(
     currentCurrency: ICurrency,
     setCurrentCurrency: (currency: ICurrency) => void }) {
 
+    const [displayRate, setDisplayRate] = useState(false);
+
     function handleCurrentCurrencyClick(banane : ICurrency) {
       setCurrentCurrency(banane)
     }
+
+    function handleDisplayRateClick() {
+      setDisplayRate(!displayRate)
+    }
+
     return(
-      <li className="currency" key={currency.code}>< button type="button" onClick={() => handleCurrentCurrencyClick(currency)} className={currency.code === currentCurrency.code ? "currency__button selected" : "currency__button"}>
-        {currency.description}
-      </button></li>
+      <li className="currency" key={currency.code}>
+        < button type="button" onClick={() => handleCurrentCurrencyClick(currency)} className={currency.code === currentCurrency.code ? "currency__button selected" : "currency__button"}>
+          {currency.description}
+          <span className= {displayRate ? "currency__rate" : "currency__rate hidden"}>
+            ({currency.rate})
+          </span>
+        </button>
+        <button type="button" className="rate-button" onClick={handleDisplayRateClick}>
+          <DisplayRateButton 
+            size={"24"}
+            color={"#bb5432"}
+            isClick={!displayRate}
+          />
+        </button>
+      </li>
     )
 }
